a1a6f037788def9678a578dfa0098e0acc4d6958
Mark <mark.davidovich@gmail.com>
2026-02-18 16:09:52 +0200

    fix bugs in AuthProvider add AuthProvider wrapper to app

--- src/auth/AuthProvider.jsx
+++ src/auth/AuthProvider.jsx
@@ -1,4 +1,6 @@
-import { createContext, useNavigate } from "react";
+import { useNavigate } from "react-router";
+import { useState, useEffect, createContext } from "react";
+import { supabase } from "../lib/supabase.js";
 
 const AuthContext = createContext(null);
 
@@ -39,45 +41,45 @@ export function AuthProvider({ onAuthReady, children }) {
     } catch (err) {
       console.log(`Error: ${err.message}`);
     }
+  };
 
-    const handleRegister = async (email, password) => {
-      const cleanEmail = email.trim().toLowerCase();
+  const handleRegister = async (email, password) => {
+    const cleanEmail = email.trim().toLowerCase();
 
-      try {
-        const { data, error } = await supabase.auth.signUp({
-          email: cleanEmail,
-          password,
-        });
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email: cleanEmail,
+        password,
+      });
 
-        if (error) {
-          throw new Error(`Failed to register! ${error.message}`);
-        }
-
-        console.log(`Registration successful!`);
-        setLoggedOnUser(data.user);
-        navigate("/");
-      } catch (err) {
-        console.log(`Error: ${err.message}`);
+      if (error) {
+        throw new Error(`Failed to register! ${error.message}`);
       }
-    };
 
-    const handleLogout = async () => {
-      try {
-        await supabase.auth.signOut();
-        setLoggedOnUser(null);
-        navigate("/login");
-        console.log("`Logged out successfully");
-      } catch (err) {
-        console.log(`Failed to log out! ${err.message}`);
-      }
-    };
+      console.log(`Registration successful!`);
+      setLoggedOnUser(data.user);
+      navigate("/");
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  };
 
-    const userCtx = {
-      loggedOnUser,
-      handleLogin,
-      handleRegister,
-      handleLogout,
-    };
+  const handleLogout = async () => {
+    try {
+      await supabase.auth.signOut();
+      setLoggedOnUser(null);
+      navigate("/login");
+      console.log("`Logged out successfully");
+    } catch (err) {
+      console.log(`Failed to log out! ${err.message}`);
+    }
+  };
+
+  const userCtx = {
+    loggedOnUser,
+    handleLogin,
+    handleRegister,
+    handleLogout,
   };
 
   return <AuthContext value={userCtx}>{children}</AuthContext>;

ecc66c2bb70a08b02e5ccca33008a9057586d924
Mark <mark.davidovich@gmail.com>
2026-02-19 18:02:13 +0200

    improve quiz logic to render new questions with a progress bar

--- src/pages/Quiz/Quiz.jsx
+++ src/pages/Quiz/Quiz.jsx
@@ -3,13 +3,16 @@ import { useNavigate } from "react-router-dom";
 import { triviaData } from "../../lib/DummyData.js";
 import QuestionCard from "../../components/QuestionCard/QuestionCard.jsx";
 import { Container, Title, Text, Progress, Flex } from "@mantine/core";
+import { TIME_PER_QUESTION, ONE_SECOND } from "../../lib/constants.js";
+import { calculatePercentage } from "../../lib/helperFunctions.js";
 
 const Quiz = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [timeLeft, setTimeLeft] = useState(5000);
+  const [currTime, setCurrTime] = useState(TIME_PER_QUESTION);
   const navigate = useNavigate();
 
   const handleNextQuestion = () => {
+    console.log("NEXT QUESTION ACTIVATED");
     if (currentIndex < triviaData.length - 1) {
       setCurrentIndex((next) => next + 1);
     } else {
@@ -18,23 +21,38 @@ const Quiz = () => {
   };
 
   useEffect(() => {
-    const timeOutId = setTimeout(() => {
-      handleNextQuestion();
-    }, timeLeft);
+    setCurrTime(TIME_PER_QUESTION);
 
-    return () => {
-      clearInterval(timeOutId);
-      setTimeLeft(5000);
-    };
+    const intervalId = setInterval(() => {
+      setCurrTime((prev) => {
+        if (prev <= ONE_SECOND) {
+          clearInterval(intervalId);
+          handleNextQuestion();
+        }
+
+        return prev - ONE_SECOND;
+      });
+    }, ONE_SECOND);
+
+    return () => clearInterval(intervalId);
   }, [currentIndex]);
+
+  const currTimePercentage = calculatePercentage(currTime, TIME_PER_QUESTION);
+  const currQuestionPercentage = calculatePercentage(currentIndex, triviaData.length - 1);
 
   return (
     <Container size="sm" py="xl">
       <Text ta="center" c="dimmed" mb="sm">
         Question {currentIndex + 1} of {triviaData.length}
       </Text>
-      <Progress mb={"lg"} value={30}></Progress>
-      <QuestionCard key={triviaData[currentIndex].id} currentQuestion={triviaData[currentIndex]} onNext={handleNextQuestion} />
+      <Progress mb={"lg"} value={currQuestionPercentage} transitionDuration={500}></Progress>
+      <QuestionCard
+        key={triviaData[currentIndex].id}
+        currentQuestion={triviaData[currentIndex]}
+        onNext={handleNextQuestion}
+        currTime={currTime}
+        currTimePercentage={currTimePercentage}
+      />
     </Container>
   );
 };

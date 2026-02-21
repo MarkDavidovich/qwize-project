23e4d9dca996f5c301dacf1fa1a2b425deea1841
Mark <mark.davidovich@gmail.com>
2026-02-21 16:15:32 +0200

    add slightly bigger font to our github links

--- src/components/ContactInfo/ContactInfo.jsx
+++ src/components/ContactInfo/ContactInfo.jsx
@@ -5,15 +5,15 @@ const ContactInfo = ({ isMobile = false }) => (
   <Flex direction={isMobile ? "column" : "row"} justify={isMobile ? "center" : "space-between"} align="center" gap="md" w="100%">
     <Text c="gray" fw={500} size={isMobile ? "sm" : "md"} ta={isMobile ? "center" : "left"}>
       Made by{" "}
-      <Anchor fw={700} href="https://github.com/galraij" target="_blank">
+      <Anchor fw={700} fz={isMobile ? "20px" : "18"} href="https://github.com/galraij" target="_blank">
         Gal
       </Anchor>
       ,{" "}
-      <Anchor fw={700} href="https://github.com/TOMLUCA7" target="_blank">
+      <Anchor fw={700} fz={isMobile ? "20px" : "18"} href="https://github.com/TOMLUCA7" target="_blank">
         Tom
       </Anchor>{" "}
       and{" "}
-      <Anchor fw={700} href="https://github.com/MarkDavidovich" target="_blank">
+      <Anchor fw={700} fz={isMobile ? "20px" : "18"} href="https://github.com/MarkDavidovich" target="_blank">
         Mark
       </Anchor>
     </Text>

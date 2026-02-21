4171de0944a940ca840240e04c4ceafe7cce3823
Mark <mark.davidovich@gmail.com>
2026-02-21 13:01:05 +0200

    add file that handles api calls for the leaderboards component

---
+++ src/lib/APILeaderboards.js
@@ -0,0 +1,47 @@
+import { supabase } from "./supabase";
+
+export const getLeaderboards = async () => {
+  try {
+    const { data, error } = supabase.from("leaderboards").select("*").order("score", { ascending: false }).limit(5);
+
+    if (error) {
+      console.error(`Error fetching top 5 results: ${error.message}`);
+    }
+
+    console.log(data);
+
+    return data;
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
+};
+
+export const updateLeaderboard = async (userEmail, newScore, correctAnswers, timeElapsed) => {
+  try {
+    const { data: existing } = await supabase.from("leaderboards").select("score").eq("user_email", userEmail).single();
+
+    if (!existing || newScore > existing.score) {
+      const { data, error } = await supabase.from("leaderboards").upsert(
+        {
+          user_email: userEmail,
+          score: newScore,
+          correct: correctAnswers,
+          time: timeElapsed,
+        },
+        {
+          onConflict: "user_email",
+        },
+      );
+
+      if (error) {
+        console.error(`Error upserting data: ${error.message}`);
+      }
+
+      return { success: true, message: "New high score!" };
+    }
+
+    return { success: false, message: "Keep trying! Not a high score" };
+  } catch (err) {
+    console.error(`Leaderboard error: ${err.message}`);
+  }
+};

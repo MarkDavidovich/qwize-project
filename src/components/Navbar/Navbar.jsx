cc4fcdba9999509e0380dfd8b22463d9e3995315
Mark <mark.davidovich@gmail.com>
2026-02-21 11:28:36 +0200

    add drawer for mobile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,50 +1,69 @@
-import { Button, Container, Flex, Group, Text } from "@mantine/core";
+import { Button, Container, Flex, Group, Text, Burger, Drawer, Stack } from "@mantine/core";
 import classes from "./Navbar.module.css";
 import { NavLink } from "react-router-dom";
 import { useAuth } from "../../auth/AuthProvider";
+import { useDisclosure } from "@mantine/hooks";
 
 export function Navbar() {
   const { loggedOnUser, handleLogout } = useAuth();
+  const [opened, { open, close, toggle }] = useDisclosure(false);
+
+  const navLinks = (
+    <>
+      <NavLink to="/" onClick={close} className={({ isActive }) => `${isActive ? classes.link + " " + classes.active : classes.link}`}>
+        Home
+      </NavLink>
+      {!loggedOnUser && (
+        <>
+          <NavLink to="/login" onClick={close} className={({ isActive }) => `${isActive ? classes.link + " " + classes.active : classes.link}`}>
+            Login
+          </NavLink>
+          <NavLink to="/register" onClick={close} className={({ isActive }) => `${isActive ? classes.link + " " + classes.active : classes.link}`}>
+            Register
+          </NavLink>
+        </>
+      )}
+      {loggedOnUser && (
+        <NavLink to="/leaderboards" onClick={close} className={({ isActive }) => `${isActive ? classes.link + " " + classes.active : classes.link}`}>
+          Leaderboards
+        </NavLink>
+      )}
+    </>
+  );
 
   return (
     <header className={classes.header}>
       <Container size="md" className={classes.inner}>
-        <Group gap={5}>
-          <NavLink to="/" className={({ isActive }) => `${isActive ? classes.link + " " + classes.active : classes.link}`}>
-            Home
-          </NavLink>
-          {!loggedOnUser && (
-            <>
-              <NavLink to="/login" className={({ isActive }) => `${isActive ? classes.link + " " + classes.active : classes.link}`}>
-                Login
-              </NavLink>
-              <NavLink to="/register" className={({ isActive }) => `${isActive ? classes.link + " " + classes.active : classes.link}`}>
-                Register
-              </NavLink>
-            </>
-          )}
-          {loggedOnUser && (
-            <NavLink to="/leaderboards" className={({ isActive }) => `${isActive ? classes.link + " " + classes.active : classes.link}`}>
-              Leaderboards
-            </NavLink>
-          )}
+        <Group gap={5} visibleFrom="xs">
+          {navLinks}
         </Group>
 
         {loggedOnUser && (
-          <Flex justify="center" align="center">
-            <Button
-              variant="transparent"
-              onClick={() => {
-                console.log("User Profile?");
-              }}
-            >
-              {loggedOnUser.email.split("@")[0]}
-            </Button>
+          <Group gap={5} visibleFrom="xs">
+            <Button variant="transparent">{loggedOnUser.email.split("@")[0]}</Button>
             <Button variant="subtle" color="red" onClick={handleLogout}>
               Log out
             </Button>
-          </Flex>
+          </Group>
         )}
+        <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
+
+        <Drawer opened={opened} onClose={close} title="Navigation" padding="md" size="sm" hiddenFrom="xs">
+          <Stack gap="md">
+            {navLinks}
+            <hr style={{ border: "0", borderTop: "1px solid #eee", width: "100%" }} />
+            {loggedOnUser && (
+              <>
+                <Text size="sm" fw={500} px="sm">
+                  User: {loggedOnUser.email}
+                </Text>
+                <Button color="red" variant="light" onClick={handleLogout}>
+                  Log out
+                </Button>
+              </>
+            )}
+          </Stack>
+        </Drawer>
       </Container>
     </header>
   );
